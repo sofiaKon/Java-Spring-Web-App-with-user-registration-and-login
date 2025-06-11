902a32015468481fdbf4acc50df2ff731321393c
sofiaKon <kondovinasofa@gmail.com>
2025-06-11 15:02:21 +0900

    Обновлен файл README.md

---
+++ src/main/java/com/example/LoginFormAplicatoin.java
@@ -0,0 +1,12 @@
+package com.example.loginform;Add commentMore actions
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginFormApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LoginFormApplication.class, args);
+    }
+}
+
